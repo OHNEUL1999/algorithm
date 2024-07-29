7dd7184bd7aab1ed21fa6072a07b83dea894720f
ohneul <139302905+OHNEUL1999@users.noreply.github.com>
2024-07-29 21:22:23 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++) {
+        for(int j = n - i; j > 0; j--) {
+            cout << " ";
+        }
+        for(int j = 2 * i - 1; j > 0; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for(int j = 2 * (n - i) - 1; j > 0; j--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
