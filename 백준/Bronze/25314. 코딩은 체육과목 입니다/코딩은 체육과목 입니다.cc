cbdfe231d906b79ef68a0a5ae654e18477ca4449
오유진 <139302905+OHNEUL1999@users.noreply.github.com>
2024-10-13 00:23:15 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    int count = N / 4;
+    for (int i = 0; i < count; i++) {
+        cout << "long ";
+    }
+    
+    cout << "int" << endl;
+    
+    return 0;
+}
