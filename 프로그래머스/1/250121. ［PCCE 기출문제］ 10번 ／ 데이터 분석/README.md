f8684e889bd807cc6ac178205779f4a2b09fd8e2
오유진 <139302905+OHNEUL1999@users.noreply.github.com>
2024-12-08 00:30:54 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.50 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 데이터 분석 - 250121 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250121) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.49 ms
+메모리: 4.21 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 08일 23:04:11
+2024년 12월 08일 00:30:51
 
 ### 문제 설명
 
--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -7,28 +7,24 @@ using namespace std;
 vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
     vector<vector<int>> answer;
     
-    // 필터링 기준이 되는 인덱스를 결정
     int filter_index = 0;
     if (ext == "code") filter_index = 0;
     else if (ext == "date") filter_index = 1;
     else if (ext == "maximum") filter_index = 2;
     else if (ext == "remain") filter_index = 3;
 
-    // 주어진 조건에 따라 데이터를 필터링하여 answer에 추가
     for (const auto& row : data) {
         if (row[filter_index] < val_ext) {
             answer.push_back(row);
         }
     }
 
-    // 정렬 기준이 되는 인덱스를 결정
     int sort_index = 0;
     if (sort_by == "code") sort_index = 0;
     else if (sort_by == "date") sort_index = 1;
     else if (sort_by == "maximum") sort_index = 2;
     else if (sort_by == "remain") sort_index = 3;
 
-    // 정렬 수행
     sort(answer.begin(), answer.end(), [sort_index](const vector<int>& a, const vector<int>& b) {
         return a[sort_index] < b[sort_index];
     });
