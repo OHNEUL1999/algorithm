9fed52e0b3950417dad21fa65c9c4368df908ef1
오유진 <139302905+OHNEUL1999@users.noreply.github.com>
2025-04-06 15:12:11 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 3.77 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -1,12 +1,20 @@
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 double solution(vector<int> arr) {
-    double answer = 0, sum = 0;
-    for(int i = 0; i < arr.size(); i++) {
-        sum += arr[i];
-    }
+    // double answer = 0, sum = 0;
+    // for(int i = 0; i < arr.size(); i++) {
+    //     sum += arr[i];
+    // }
+    // answer = sum / arr.size();
+    // return answer;
+    
+    double answer = 0;
+    
+    double sum = accumulate(arr.begin(), arr.end(), 0.0);
     answer = sum / arr.size();
+    
     return answer;
 }
